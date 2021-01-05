e93bf1dc0435ef2b6a47e960baf1d2c8e26b8cea
Ian Hancock <northroadian@gmail.com>
2021-01-05 05:59:37 -0600

    relearning arrow functions

---
+++ arrow.js
@@ -0,0 +1,14 @@
+// old function format. 
+//const divide = function (x,y) {
+ //   return x/y;
+
+//}
+
+
+const divide = (x,y) =>{
+    return x/y ; 
+}
+
+const square = (x) => {
+    return x * x ; 
+ }

051b05a4f01602e0768c76ddcde80fc6eb3161bd
Ian Hancock <northroadian@gmail.com>
2021-02-02 07:08:24 -0600

    more functions practice higher order function practice

--- functionFunction.js
+++ functionFunction.js
@@ -47,3 +47,106 @@ makeLunch({main : 'waffels',side: 'eggs' , drink: 'coffee'})
 // this is one argument "options" that is and object that can contain multiple values. 
 
 /// PICK UP ON 4:33 of video. 
+
+// Rest parameters
+function makeDinner(...arguments){
+    return `Dinner includes the following items : ${arguments}`
+}
+makeDinner('cheeseburger','fries','beer','pickles')
+console.log(makeDinner)
+
+// this will return "Dinner includes the following items : cheeseburger','fries','beer','pickles' "
+// this is cool it will spread operator it and return whatever we put in as arguments
+
+
+
+// Arrow Function 
+
+// Regular function 
+const makeBeer = function beerFun(qty){
+    return 'beer'.repeat.qty
+}
+
+//Arrow function 
+// if only one return statement no need for curly braces
+// if more than one return statement you need {}
+
+const makeWine = (qty) => {
+  return  "🍷".repeat(qty);
+}
+// other example. 
+function Dog(){
+    this.breed = "wolf"
+
+    setTimeout(() => {
+        console.log(this.breed); wolf
+    }, 0)
+}
+
+//Impure functions 
+// this is operating on values outside its scope
+let x = 0 ;
+
+const impure = () => {
+    x++; 
+    return x**2; 
+}
+
+// Pure functions
+// uses variables inside its scope and only mutates variables in its local scope
+// does not create side effects for other code. 
+// Better to do in general 
+// Will always produce the same output given the same input.
+// Easier to test 
+
+
+const pure = (x)=> {
+   return x**2 
+}
+console.log(pure(5))
+// this will = 25 
+// this is 5 to the 2nd power 
+
+// Higher order functions 
+// this is a callback function. 
+let haveFun = () => console.log('fun'); 
+// below setTime out is taking the function have fun as a parameter and 
+//the 50 is 50 milliseconds as a parameter
+
+setTimeout(haveFun,50); 
+
+// or like this...  call back function..
+// this will run the console.log 'fun' on a setTimeout of 5 seconds (5000 millisecods = 5 seconds)
+// on a 5 second delay. pretty cool this will be use I think for page items to load and heros and carousels 
+let haveFun = 
+setTimeout(() => console.log('fun'),5000)
+
+// replacing traditional "For Loops"
+// Traditional loops here 
+const arr = [1,2,3,4]
+
+const squared = [];
+
+for(const x of arr ){
+    squared.push(x**2); 
+}
+// the result is 
+[1,4,9,16]
+console.log(squared);
+
+// new style loop 
+const squared2 = arr.map(x => x**2);
+console.log(squared2)
+
+// to reuse this code set it up as a function this way. 
+// you are taking the function stored inside square it 
+// putting it in as a parameter in squared 2 
+// mapping over the arr variable inside the squared2 variable 
+// returning the squared2 variable
+const arr = [1,2,3,4]
+const squareIt = x => x**2; 
+const squared2 = arr.map(squareIt)
+//call squared 2 
+
+squared2 = [1,4,9,16]
+
